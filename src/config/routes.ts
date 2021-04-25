3b0be8d5ebfb293808521926f6f91698f5fa0d7b
Joao Pedrozo <jp.pedrozo@hotmail.com>
2021-04-25 03:49:07 -0300

    style: fix style of config folder

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,17 +1,17 @@
-import { Router } from "express";
-import UserController from "../controllers/UserController";
-import CategoryController from "../controllers/CategoryController";
+import { Router } from 'express';
+import UserController from '../controllers/UserController';
+import CategoryController from '../controllers/CategoryController';
 
 const router = Router();
 const userController = new UserController();
 const categoryController = new CategoryController();
 
-router.get("/users/find", userController.find);
-router.post("/users/add", userController.add);
-router.post("/users/deposit", userController.deposit);
-router.post("/users/delete", userController.deleteUser);
-router.post("/users/transfer", userController.transfer);
-router.get("/category/find", categoryController.find);
-router.post("/category/add", categoryController.add);
+router.get('/users/find', userController.find);
+router.post('/users/add', userController.add);
+router.post('/users/deposit', userController.deposit);
+router.post('/users/delete', userController.deleteUser);
+router.post('/users/transfer', userController.transfer);
+router.get('/category/find', categoryController.find);
+router.post('/category/add', categoryController.add);
 
 export default router;
