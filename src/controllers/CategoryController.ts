11678b51e0dfc9fbd51c57b4a7d23dc35ff1bb9d
Bruno <machado.bruno22@gmail.com>
2021-05-03 08:35:21 -0300

    delete category

--- src/controllers/CategoryController.ts
+++ src/controllers/CategoryController.ts
@@ -20,6 +20,20 @@ class CategoryController {
           console.log(err);
         }
       }
+      
+      async deleteCategory(request: Request, response: Response) {
+        const name = request.body.name;
+        const filter = { name : name };
+        const category = await category.findOneAndDelete(filter);
+    
+        if (name == null) {
+          response
+            .status(400)
+            .json("Não existe nenhuma Categoria com esse Nome para ser deletada");
+        } else {
+          response.status(200).json("Categoria deletada com sucesso!");
+        }
+      }
 }
 
 export default CategoryController;
